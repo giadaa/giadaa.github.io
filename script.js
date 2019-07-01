eb2b18c5dd6fe785cb8247fd76985c5485d5a6f7
Jade <jadelouiseholden@gmail.com>
2019-07-01 13:06:13 +0100

    JS 2

--- script.js
+++ script.js
@@ -1,53 +1,45 @@
-
+/*
 //script for destinations card filter
 
 //europe
 $("#show-europe").click(function(){
- // $(".asia").show();
- // $(".south-america").show();
- // $(".north-america").show();
-  $(".europe").show();
   $(".asia").hide();
   $(".south-america").hide();
   $(".north-america").hide();
- // $(".europe").appendTo(".row1");
+  $(".europe").show();
+
 });
 
 //north america
 $("#show-n-america").click(function(){
- // $(".asia").show();
-  //$(".south-america").show();
-  $(".north-america").show();
-  //$(".europe").show();
   $(".asia").hide();
   $(".south-america").hide();
   $(".europe").hide();
- // $(".north-america").appendTo(".row1");
+  $(".north-america").show();
 });
 
 //south america
 $("#show-s-america").click(function(){
-//  $(".asia").show();
-  $(".south-america").show();
- // $(".north-america").show();
- // $(".europe").show();
   $(".asia").hide();
   $(".north-america").hide();
   $(".europe").hide();
-  //$(".south-america").appendTo(".row1");
+  $(".south-america").show();
 });
 
 //asia and oceania
 $("#show-asia").click(function(){
-  $(".asia").show();
- // $(".south-america").show();
- // $(".north-america").show();
- // $(".europe").show();
   $(".north-america").hide();
   $(".south-america").hide();
   $(".europe").hide();
- // $(".asia").appendTo(".row1");
+  $(".asia").show();
 });
+*/
+
+
+
+
+
+
 
 //reset filters
 $('#reset-filters').click(function(){
